3c89aad207d5f6401942e763f69aaf546268fa63
Quách Bảo Quân <quachbaoquan123@gmail.com>
2023-09-19 16:59:38 +0700

    chore: add file vercel.json

--- config/asgi.py
+++ config/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 from socket_io.views import app as socket_app
 from channels.routing import ProtocolTypeRouter
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
