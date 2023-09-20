470834e287825abb77f805ee9cecc0cdf0f3231a
Quách Bảo Quân <quachbaoquan123@gmail.com>
2023-09-20 11:10:35 +0700

    chore: config for vercel v6

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
-echo " BUILD START"
+echo "BUILD START"
+
+echo "Upgrading pip"
+python3 -m pip install --upgrade pip
+
+echo "Building the project..."
 python3 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
 python3 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+echo "BUILD END"
