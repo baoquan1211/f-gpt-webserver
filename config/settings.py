91872348a5d4db068ee30647500b5e0fe13fac2f
Quách Bảo Quân <quachbaoquan123@gmail.com>
2023-09-22 13:14:31 +0700

    fix: fix database issues

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv("DATABASE_NAME"),
         "USER": os.getenv("DATABASE_USER"),
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
