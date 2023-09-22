dd224a4e041d14c3f666ea992c752f1685b9ef2d
Quách Bảo Quân <quachbaoquan123@gmail.com>
2023-09-22 11:31:48 +0700

    build: test no secretkey error

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-o1(5&5-537eaf+x1aw-usrt&m%15(*qrjd&$%)gya!8^a2x69d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
